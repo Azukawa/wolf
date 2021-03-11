5e3313ca185ca090521b560f3464a8c548523077
Alero <alero@Ashanti.local>
2021-03-11 12:54:06 +0200

    some files added

---
+++ wold3d.c
@@ -0,0 +1,16 @@
+#include "../minilibx-linux-master/mlx.h"
+#include "../Libft/inc/libft.h"
+#include <stdio.h>
+
+
+int			readmap()
+{
+	
+	return(0);
+}
+
+int			main(int argc, char** argv)
+{
+	
+	return (1);
+}
