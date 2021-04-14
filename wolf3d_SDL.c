fc059c91218743480d80f049a1ce64e04c9b1311
Alero <alero@Ashanti.local>
2021-04-14 19:28:06 +0300

    Program now draws the map on screen. Something wrong with mallocing of the 2d array for the map, as trying to free will cause segfault.

--- wolf3d_SDL.c
+++ wolf3d_SDL.c
@@ -52,6 +52,7 @@ char	*linetogrid(char *ret, char *out, int x)
 		}
 		a++;
 	}
+	ret[i] = '\0';
 	return(ret);
 }
 
@@ -131,6 +132,8 @@ int		initmap(t_map *s)
 		s->map[i][s->x] = '\0';
 		i++;
 	}
+	s->map[s->y][0] = '\0';
+//	ft_memset(s->map, 0, sizeof(char *) * s->y * s->x);
 	ft_strclr(s->map[s->y]);
 	return(0);
 }
@@ -139,6 +142,7 @@ int		initmap(t_map *s)
  *	Simple mother function for
  *	commanding the seperate map
  *	reading functions.
+ *	final 2d char will be map[y][x]
  */
 int			buildmap(char *str, t_map *s)
 {	
@@ -172,18 +176,58 @@ void	initSDL(t_app *app)
 		exit(printf("Failed to create renderer: %s\n", SDL_GetError()));
 }
 
+void			free2darr(t_map *map)
+{
+	int y;
+	y = 0;
+	while(y < map->y)
+	{
+		free(map->map[y]);
+		y++;
+	}
+	free(map->map);
+}
+
 void	cleanup(t_app *app)
 {
+	printf("555, %p\n", app->renderer);
 	SDL_DestroyRenderer(app->renderer);
-	
+	printf("666\n");
 	SDL_DestroyWindow(app->window);
+	printf("777\n");
+}
 
-	SDL_Quit();
+void		drawmap(t_map *map, t_app *app)
+{	
+	if (map->x == 0 || map->y == 0)
+		return ;
+	int	x, y, square_x, square_y;
+	x = y = 0;
+	square_x = SCREEN_WIDTH / map->x;
+	square_y = SCREEN_HEIGHT / map->y;
+	SDL_SetRenderDrawColor(app->renderer, 0, 0, 0, 0);
+	SDL_RenderClear(app->renderer);
+	SDL_SetRenderDrawColor(app->renderer, 255, 0, 0, 255);
+	printf("mapx=%d\nmapy=%d\n", map->x, map->y);
+	while(y < SCREEN_HEIGHT)
+	{	
+		while(x < SCREEN_WIDTH)
+		{
+			if (map->map[y / square_y][ x / square_x] == '0')
+				SDL_SetRenderDrawColor(app->renderer, 255, 255, 255, 255);
+			else
+				SDL_SetRenderDrawColor(app->renderer, 0, 0, 0, 0);
+			SDL_RenderDrawPoint(app->renderer, x, y);
+			x++;
+		}
+		x = 0;
+		y++;
+	}
 }
 
 int			main(int argc, char** argv)
 {
-	t_map	s;
+	t_map		s;
 	t_app		app;
 
 	if (argc != 2)
@@ -191,15 +235,25 @@ int			main(int argc, char** argv)
 	ft_memset(&app, 0, sizeof(app));
 	ft_memset(&s, 0, sizeof(s));
 	initSDL(&app);
+	printf("xxx\n");
 	buildmap(argv[1], &s);
+	printf("yyy\n");
 	ft_print2dcarr(s.map);
-	app.screenSurface = SDL_GetWindowSurface( app.window );
-	SDL_FillRect(app.screenSurface, NULL, SDL_MapRGB(app.screenSurface->format, 0x00, 0xFF, 0xFF));
-	SDL_UpdateWindowSurface(app.window);
+	printf("zzz\n");
+	app.screenSurface = SDL_GetWindowSurface(app.window);
+	drawmap(&s, &app);
+	printf("111\n");
+	SDL_RenderPresent(app.renderer);
 	SDL_Delay(3000);
+	printf("2222\n");
+//	free2darr(&s);
+//	int i = 0;
+//	while(i < s.y)
+//		free(s.map[i++]);
+//	free(s.map);
+//	cleanup(&app);
+	printf("3333\n");
 	SDL_DestroyWindow(app.window);
-	cleanup(&app);
-	free(s.map);
 	printf("Ran untill end\n");
 	return (1);
 }
