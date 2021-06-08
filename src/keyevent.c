ee83ab02c0dbb25a06f33ad6e1ae9b65e84498f8
Alero <alero@Ashanti.local>
2021-06-08 18:42:13 +0300

    read map and keyevent now in norme.

--- src/keyevent.c
+++ src/keyevent.c
@@ -6,38 +6,48 @@
 /*   By: eniini <eniini@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 10:53:01 by alero             #+#    #+#             */
-/*   Updated: 2021/06/04 16:09:46 by alero            ###   ########.fr       */
+/*   Updated: 2021/06/08 18:35:00 by alero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/wolf.h"
-
-void		keyevent(t_app *app, SDL_Event *e)
+void	key_forward(double *new_pos_x, double *new_pos_y, t_app *app)
 {
+	*new_pos_x = app->player.pos_x + cos(app->player.angle * \
+			RAD_CON) * app->player.move_u;
+	*new_pos_y = app->player.pos_y + sin(app->player.angle * \
+			RAD_CON) * app->player.move_u;
+	app->player.pos_x = *new_pos_x;
+	app->player.pos_y = *new_pos_y;
+}
+
+void	key_backward(double *new_pos_x, double *new_pos_y, t_app *app)
+{
+	*new_pos_x = app->player.pos_x - cos(app->player.angle * \
+		RAD_CON) * app->player.move_u;
+	*new_pos_y = app->player.pos_y - sin(app->player.angle * \
+		RAD_CON) * app->player.move_u;
+	app->player.pos_x = *new_pos_x;
+	app->player.pos_y = *new_pos_y;
+}
+
+void	keyevent(t_app *app, SDL_Event *e)
+{
+	double	new_pos_x;
+	double	new_pos_y;
+
 	while (SDL_PollEvent(e))
 	{
-		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_ESCAPE)
+		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_x)
 			app->run = 0;
-	//MOVEMENT AND COLLISION CHECKS
 		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_w)
-		{
-			double new_pos_x = app->player.pos_x + cos(app->player.angle * RAD_CON) * app->player.move_u;
-			double new_pos_y = app->player.pos_y + sin(app->player.angle * RAD_CON) * app->player.move_u;
-				app->player.pos_x = new_pos_x;
-				app->player.pos_y = new_pos_y;
-		}
+			key_forward(&new_pos_x, &new_pos_y, app);
 		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_s)
-		{
-			double new_pos_x = app->player.pos_x - cos(app->player.angle * RAD_CON) * app->player.move_u;
-			double new_pos_y = app->player.pos_y - sin(app->player.angle * RAD_CON) * app->player.move_u;
-				app->player.pos_x = new_pos_x;
-				app->player.pos_y = new_pos_y;
-		}
+			key_backward(&new_pos_x, &new_pos_y, app);
 		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_a)
 			app->player.angle -= app->player.rotation_u;
 		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_d)
 			app->player.angle += app->player.rotation_u;
-	// DRAW UI OR NOT
 		if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_i)
 		{
 			if (app->draw_ui)
@@ -46,5 +56,4 @@ void		keyevent(t_app *app, SDL_Event *e)
 				app->draw_ui = TRUE;
 		}
 	}
-
 }
