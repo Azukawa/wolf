da46b10f6ac9375cf8bc89c36a293f0f0db8e0fa
Alero <alero@Ashanti.local>
2021-06-08 15:43:49 +0300

    drawmap.c function now in norme. Removed Null protection: REMEMBER TO PROTECT ELSEWHERE!

--- src/drawmap.c
+++ src/drawmap.c
@@ -6,7 +6,7 @@
 /*   By: eniini <eniini@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 11:01:28 by alero             #+#    #+#             */
-/*   Updated: 2021/06/02 11:42:18 by alero            ###   ########.fr       */
+/*   Updated: 2021/06/08 15:40:33 by alero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
  *		and end point is the lower right corner.
  */
 static void	drawsquare(t_point start, t_point end, unsigned int *buffer, \
-uint32_t color)
+		uint32_t color)
 {
 	int	x;
 
@@ -39,40 +39,27 @@ void	drawmap(t_map *map, t_app *app)
 {
 	t_point		start;
 	t_point		end;
-	int			square_x;
-	int			square_y;
-	int			x;
-	int			y;
-	//int			color, 
-	x = 0;
-	y = 0;
-	square_x = (SCREEN_WIDTH / map->w) / 4;
-	square_y = (SCREEN_HEIGHT / map->h) / 4;
-	if (map)
-	{	
-		while (y < map->h)
+	t_point		square;
+	t_point		index;
+
+	ft_memset(&index, 0, sizeof(t_point));
+	square.x = (SCREEN_WIDTH / map->w) / 4;
+	square.y = (SCREEN_HEIGHT / map->h) / 4;
+	while (index.y < map->h)
+	{
+		while (index.x < map->w)
 		{
-			while (x < map->w)
-			{
-				//if(map->map[y][x] == '0')
-				//	color = 0xffffffff;
-				//else
-				//	color = 0xff;
-				start.x = x * square_x;
-				start.y = y * square_y;
-				end.x = (x + 1) * square_x;
-				end.y = (y + 1) * square_y;
-				//if (color != 0)
-				if (map->map[y][x] == '0')
-					drawsquare(start, end, app->buffer, \
-ft_argb_lerp(0xff7f0055, ft_argb_lerp(0xff, \
-0xff7dff00, (float)x / map->w), (float)y / map->h));
-				//else
-					//drawsquare(start, end, app->buffer, 0x0);
-				x++;
-			}
-			y++;
-			x = 0;
+			start.x = index.x * square.x;
+			start.y = index.y * square.y;
+			end.x = (index.x + 1) * square.x;
+			end.y = (index.y + 1) * square.y;
+			if (map->map[index.y][index.x] == '0')
+				drawsquare(start, end, app->buffer, \
+ft_argb_lerp(0xff7f0055, ft_argb_lerp(0xff, 0xff7dff00, \
+	(float)index.x / map->w), (float)index.y / map->h));
+			index.x++;
 		}
+		index.y++;
+		index.x = 0;
 	}
 }
