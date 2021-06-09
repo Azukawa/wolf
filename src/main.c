b9c7713a889cb7057872393a5dee3e3e2604040c
Alero <alero@Ashanti.local>
2021-06-09 14:54:10 +0300

    main.c is now in norme and so is the whole project. Lets go wolfpack! AWOOOOOOO

--- src/main.c
+++ src/main.c
@@ -6,55 +6,24 @@
 /*   By: eniini <eniini@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 11:03:10 by alero             #+#    #+#             */
-/*   Updated: 2021/06/08 11:10:13 by eniini           ###   ########.fr       */
+/*   Updated: 2021/06/09 14:52:01 by alero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/wolf.h"
-
-int			main(int argc, char** argv)
+static void	mainloop(t_map *s, t_app *app, SDL_Event *e)
 {
-	t_map		*s;
-	t_app		*app;
-	SDL_Event	e;
-	t_map_player	player;
-	
-	if (argc != 2)
-		ft_printf("Wrong number of arguments.\n");
-	ft_memset(&player, 0, sizeof(t_map_player));
-	player.p.x = player.p.y = 100;
-	s = ft_memalloc(sizeof(*s));
-	app = ft_memalloc(sizeof(*app));
-	app->run = 1;
-	init_SDL(app);
-	init_player_vars(app);
-	ft_printf("xxx\n");
-	buildmap(argv[1], s);
-	//ft_print2dcarr(s->map);
-	app->screenSurface = SDL_GetWindowSurface(app->window);
-
-	t_gfxinfo *bmp = ft_load_bmp("resources/testpattern.bmp");
-	if (!bmp)
-		ft_printf("failed to load bmp file");
-	app->rc.walltex = bmp;
-	while(app->run)
+	while (app->run)
 	{
-		keyevent(app, &e);
-
-		ft_bzero(app->buffer, SCREEN_HEIGHT * SCREEN_WIDTH); //clear buffer
-
+		keyevent(app, e);
+		ft_bzero(app->buffer, SCREEN_HEIGHT * SCREEN_WIDTH);
 		check_collision(app, s);
 		raycast(app, s);
 		if (app->draw_ui)
 			drawmap(s, app);
-			/*
-			*	SDL_LockTexture gives us the tex_pitch which is the 'true' size of drawn screen width
-			*	(how many pixels are drawn in one level).
-			*/
-//		keyevent(app, &e, &player, s);
-			//drawplayer(app, &player, s);
-		if(SDL_LockTexture(app->texture, NULL, (void **)&app->tex, &app->tex_pitch) < 0)
-				ft_getout(SDL_GetError());
+		if (SDL_LockTexture(app->texture, NULL, (void **)&app->tex, \
+					&app->tex_pitch) < 0)
+			ft_getout(SDL_GetError());
 		ft_memcpy(app->tex, app->buffer, SCREEN_HEIGHT * app->tex_pitch);
 		SDL_UnlockTexture(app->texture);
 		if (SDL_RenderCopy(app->renderer, app->texture, NULL, NULL) < 0)
@@ -62,14 +31,32 @@ int			main(int argc, char** argv)
 		fps_counter(app);
 		SDL_RenderPresent(app->renderer);
 	}
-	cleanup(app);
+}
 
+int	main(int	argc, char	**argv)
+{
+	t_map			*s;
+	t_app			*app;
+	SDL_Event		e;
+	t_gfxinfo		*bmp;
+
+	if (argc != 2)
+		ft_printf("Wrong number of arguments.\n");
+	s = ft_memalloc(sizeof(*s));
+	app = ft_memalloc(sizeof(*app));
+	app->run = 1;
+	init_SDL(app);
+	init_player_vars(app);
+	buildmap(argv[1], s);
+	app->screenSurface = SDL_GetWindowSurface(app->window);
+	bmp = ft_load_bmp("resources/testpattern.bmp");
+	app->rc.walltex = bmp;
+	mainloop(s, app, &e);
+	cleanup(app);
 	ft_free_arr(s->map);
 	free(s);
 	free(app->buffer);
 	free(app);
-
-	//SDL_DestroyWindow(app->window);
 	ft_printf("Ran untill end\n");
 	return (1);
 }
