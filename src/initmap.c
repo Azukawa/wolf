469c269a1858fa3a9b4051a670da0eac6ed71177
Alero <alero@Ashanti.local>
2021-06-09 16:58:09 +0300

    initmap.c now more cause leaks

--- src/initmap.c
+++ src/initmap.c
@@ -6,7 +6,7 @@
 /*   By: eniini <eniini@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 10:54:43 by alero             #+#    #+#             */
-/*   Updated: 2021/06/08 15:49:40 by alero            ###   ########.fr       */
+/*   Updated: 2021/06/09 16:51:11 by alero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	initmap(t_map *s)
 
 	i = 0;
 	s->map = (char **)malloc(sizeof(char *) * (s->h + 1));
-	while (i <= s->h)
+	while (i < s->h)
 	{
 		s->map[i] = (char *)malloc(sizeof(char) * (s->w) + 1);
 		ft_bzero(s->map[i], s->w + 1);
